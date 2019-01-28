b9549d3eb598a5f1ea6a026e97b96b92c59347e1
andriytrundey <t_andriy@ukr.net>
2019-01-28 13:34:32 +0200

    cw6

---
+++ Homework/classwork6/src/map/Main.java
@@ -0,0 +1,56 @@
+package map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Map< Integer, String> employeeMap = new HashMap<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		employeeMap.put(1, "Vasya");
+		employeeMap.put(2, "Ivan");
+		employeeMap.put(3, "Kolya");
+		employeeMap.put(4, "Anna");
+		employeeMap.put(5, "Oksana");
+		employeeMap.put(6, "Nastya");
+		employeeMap.put(7, "Jylia");
+		
+		System.out.println(employeeMap);
+		System.out.println("Input key");
+
+			int key = Integer.valueOf(br.readLine());
+			if(employeeMap.containsKey(key)) {
+			System.out.println((String) employeeMap.get(key));
+		}else {
+			System.out.println("This key is apsent");
+		}
+		
+
+		System.out.println("Input name");
+		String name = br.readLine();
+		
+			   if(employeeMap.containsValue(name)) {				
+				   System.out.println(employeeMap.entrySet());
+				   Iterator<Entry<Integer, String>> iterator = employeeMap.entrySet().iterator();
+				   while (iterator.hasNext()) {
+					   Entry<Integer, String> next = iterator.next();
+					if(next.getValue().equals(name)) {
+						System.out.println(next.getKey());
+					}
+					
+				}
+			   
+			   }else {
+					System.out.println("This name apsent");
+				}
+
+	}
+
+}
