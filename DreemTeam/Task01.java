0e76697ea81074b0ba42bcf1105a87cd7cad4aae
VRomankiv <vromankiv@gmail.com>
2019-02-13 19:13:11 +0200

    Update Task01.java

--- DreemTeam/Task01.java
+++ DreemTeam/Task01.java
@@ -3,6 +3,7 @@ package tasks;
 public class Task01 {
 	public static long fibonacci(int n) {
 		if (n <= 1) { return n;	} 
-		else { return fibonacci(n - 1) + fibonacci(n - 2); }
+		else if (n > 1) { return fibonacci(n - 1) + fibonacci(n - 2); }
+		else { return 0; }
 	}
 }
