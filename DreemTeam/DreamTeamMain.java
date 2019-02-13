109471f4ba0f779a058f572f41fe1ed7318bc3ca
Yaroslav Senyshyn <senyshyn.yaroslav@icloud.com>
2019-02-13 17:37:44 +0200

    add 6 & main

---
+++ DreemTeam/DreamTeamMain.java
@@ -0,0 +1,16 @@
+package Chellange;
+
+import java.util.Scanner;
+
+public class DreamTeamMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		calculateNumberOfWords.calculateNumberOfWords(sc);
+		
+		
+
+	}
+
+}
