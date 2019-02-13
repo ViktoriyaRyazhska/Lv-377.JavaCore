e999e8c5893d6ec8fb3ce9ba9d6fce46da5a9ce0
Yaroslav Senyshyn <senyshyn.yaroslav@icloud.com>
2019-02-13 22:37:30 +0200

    Update Task11.java
    perfect work try it

--- DreemTeam/Task11.java
+++ DreemTeam/Task11.java
@@ -4,31 +4,29 @@ import java.util.Scanner;
 
 public class Task11 {
 	public static void checkNumberCard(Scanner sc) {
-		String cardNumber = sc.nextLine();
+		System.out.println("Info about quantity digits: AmericanExpress-15; Visa & MasterCard-16");
+		System.out.println("Pleas input your number");
+		String cNum = sc.nextLine();
 		int sum = 0;
 		boolean revision = false;
-		String systemCard = "";
-		int systemCardQ = 0;
-		if (cardNumber.length() == 16 || cardNumber.length() == 12) {
-
-			for (int i = cardNumber.length() - 1; i >= 0; i--) {
-				int number = Integer.parseInt(cardNumber.substring(i, i + 1));
-
-				if (i == 1) {
-					systemCard = (cardNumber.substring(i, i + 1));
-				}
-				if (i == 0) {
-					systemCard = systemCard.concat(cardNumber.substring(i, i + 1));
-				}
-
-				if (i == 0) {
-					systemCardQ = Integer.parseInt(cardNumber.substring(i, i + 1));
-					if (systemCardQ == 4) {
-						System.out.println("MasterCard");
-					} else if (systemCardQ == 5) {
-						System.out.println("Visa");
-					}
-				}
+	
+		if (((cNum.length()==16)&&(cNum.charAt(0)=='5')) 
+				||((cNum.length()==16)&&(cNum.charAt(0)=='4')) 
+				||((cNum.length()==15&&(cNum.charAt(0)=='3')&&(cNum.charAt(1)=='7')))) {
+			switch (cNum.charAt(0)) {
+			case '5':
+				System.out.print("Visa: ");
+				break;
+			case '4':
+				System.out.print("MasterCard: ");
+				break;
+			default:
+				System.out.print("American Expres: ");
+				break;
+			}
+			
+			for (int i = cNum.length() - 1; i >= 0; i--) {
+				int number = Integer.parseInt(cNum.substring(i, i + 1));
 				if (revision) {
 					number *= 2;
 					if (number > 9) {
@@ -38,16 +36,13 @@ public class Task11 {
 				sum += number;
 				revision = !revision;
 			}
-			if (systemCard.equals("73")&&cardNumber.length()==12) {
-				System.out.println("American Expres");
-			}
 			if (sum % 10 == 0) {
-				System.out.println("Credit number correct");
+				System.out.println("Credit card number correct. Congratulation!");
 			} else {
-				System.out.println("Credit number incorrect");
+				System.out.println("you try inpyt, but number incorrect. Input again.");
 			}
-		} else {
-			System.out.println("Incorect quantity number");
-		}
+	} else {
+		System.out.println("Incorect quantity or value.");
 	}
+}
 }
